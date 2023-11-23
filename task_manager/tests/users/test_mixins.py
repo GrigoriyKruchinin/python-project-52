ad73c548bfecec84fcae1db7cdf148ccf48ca0cf
GrigoriyKruchinin <GrigoriyKruchinin>
2023-11-23 13:40:07 +0100

    finished test_mixins.py

--- task_manager/tests/users/test_mixins.py
+++ task_manager/tests/users/test_mixins.py
@@ -4,11 +4,16 @@ from django.urls import reverse
 from django.contrib.messages import get_messages
 from django.utils.translation import gettext_lazy as _
 
+
 class UserPermissionMixinTest(TestCase):
     def setUp(self):
-        self.user = get_user_model().objects.create(
+        self.user = get_user_model().objects.create_user(
             username='Sokol',
             password='123'
+        )
+        self.other_user = get_user_model().objects.create_user(
+            username='OtherUser',
+            password='456'
         )
         self.client = Client()
 
@@ -23,8 +28,18 @@ class UserPermissionMixinTest(TestCase):
         self.assertRedirects(response, reverse('login'))
 
     def test_dispatch_for_attempt_to_change_other_user(self):
-        pass
+        self.client.login(username='Sokol', password='123')
+        url = reverse('user_update', kwargs={'pk': self.other_user.pk})
+        response = self.client.get(url)
+        messages = list(get_messages(response.wsgi_request))
 
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), _("You don't have permissions to modify another user."))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('users'))
 
     def test_dispatch_for_authorized_user(self):
-        pass
+        self.client.login(username='Sokol', password='123')
+        url = reverse('user_update', kwargs={'pk': self.user.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
