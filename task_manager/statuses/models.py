2c8496bf40474fd37a2aaa171f0844e2eb75057b
GrigoriyKruchinin <GrigoriyKruchinin>
2023-11-24 10:44:19 +0100

    made model Status

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,3 +1,23 @@
-# from django.db import models
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class Status(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name=_("Name"),
+        unique=True
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("Date of Creation")
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Status")
+        verbose_name_plural = _("Statuses")
+        ordering = ['created_at', 'name']
