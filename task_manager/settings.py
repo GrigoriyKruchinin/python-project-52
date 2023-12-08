914d27e198dd4058cf392ecf7b61460aa27a3a81
GrigoriyKruchinin <GrigoriyKruchinin>
2023-12-08 13:39:07 +0100

    update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -107,15 +107,17 @@ if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+    db_from_env = dj_database_url.config(conn_max_age=600)
+    DATABASES['default'].update(db_from_env)
 else:
     DATABASE_URL = os.getenv('DATABASE_URL')
     DATABASES = {
         'default': dj_database_url.config(
             default=DATABASE_URL,
-            conn_max_age=500,
+            conn_max_age=600,
         )
     }
 
