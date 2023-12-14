4160bc5a35a17be7b7266cc498650cc1029a96f9
GrigoriyKruchinin <GrigoriyKruchinin>
2023-12-14 22:30:58 +0100

    update static

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -151,9 +151,13 @@ LOCALE_PATHS = [BASE_DIR / 'locale']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "static_root"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "task_manager" / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
